f216ed9c8697a1c2b02b6e00ca0c51f7e461ffcc
Oscar Moran <oscar91moran@gmail.com>
2024-03-01 09:44:21 +0000

    fix

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,179 +1,53 @@
-const { db } = require('@vercel/postgres');
-const {
-    invoices,
-    customers,
-    revenue,
-    users,
-} = require('../app/lib/placeholder-data.js');
-const bcrypt = require('bcrypt');
+//IMPORT
+import dotenv from 'dotenv'
+import pg from 'pg'
 
-async function seedUsers(client) {
-    try {
-        await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-        // Create the "users" table if it doesn't exist
-        const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS users (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-      );
-    `;
+//CONFIG
+dotenv.config()
+export const db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
-        console.log(`Created "users" table`);
+//CREATE TABLE
+db.query(`CREATE TABLE IF NOT EXISTS mangas(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    url TEXT
+)`)
 
-        // Insert data into the "users" table
-        const insertedUsers = await Promise.all(
-            users.map(async (user) => {
-                const hashedPassword = await bcrypt.hash(user.password, 10);
-                return client.sql`
-        INSERT INTO users (id, name, email, password)
-        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-        ON CONFLICT (id) DO NOTHING;
-      `;
-            }),
-        );
+// INSERT TABLE
+const mangas = [
+    { title: 'Naruto', author: 'Masashi Kishimoto', url: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781974740932/naruto-shippuden-the-official-coloring-book-9781974740932_hr.jpg' },
+    { title: 'Boruto', author: 'Masashi Kishimoto and Mikio Ikemoto', url: 'https://m.media-amazon.com/images/I/71xdH9mKNuL._AC_UF894,1000_QL80_.jpg' },
+    { title: 'One piece', author: 'Eiichirō Oda', url: 'https://www.escapistmagazine.com/wp-content/uploads/2023/08/onepieceranking1.jpg?resize=800%2C400' },
+    { title: 'Bleach', author: 'Tite Kubo', url: 'https://wallpapers.com/images/featured/bleach-anime-pictures-x5ildyvb7u41kblh.jpg' },
+    { title: 'Boku no Hero Academia', author: 'Kōhei Horikoshi', url: 'https://static.wikia.nocookie.net/whumpapedia/images/e/e6/MV5BNmQzYmE2MGEtZjk4YS00YmVjLWEwZWMtODRkMjc4MTM5N2I3XkEyXkFqcGdeQXVyNTAyODkwOQ%40%40._V1_.jpg/revision/latest?cb=20200605082628' },
+    { title: 'Death note', author: 'Tsugumi Ohba and Takeshi Obata', url: 'https://m.media-amazon.com/images/M/MV5BNjRiNmNjMmMtN2U2Yi00ODgxLTk3OTMtMmI1MTI1NjYyZTEzXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg' },
+    { title: 'One-Punch Man', author: 'Yusuke Murata and ONE', url: 'https://i.pinimg.com/236x/c6/42/d3/c642d39d2fe134a43381460b8d6b7dd7.jpg' },
+    { title: 'Demon slayer', author: 'Koyoharu Gotōge', url: 'https://m.media-amazon.com/images/M/MV5BODI2NjdlYWItMTE1ZC00YzI2LTlhZGQtNzE3NzA4MWM0ODYzXkEyXkFqcGdeQXVyNjU1OTg4OTM@._V1_.jpg' },
+    { title: 'Fullmetal Alchemist', author: 'Hiromu Arakawa', url: 'https://m.media-amazon.com/images/M/MV5BZmEzN2YzOTItMDI5MS00MGU4LWI1NWQtOTg5ZThhNGQwYTEzXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg' },
+    { title: 'Dragon Ball', author: 'Akira Toriyama', url: 'https://m.media-amazon.com/images/M/MV5BY2I2MzI1ODYtMWRlOS00MzdhLWEyOWEtYWJhNmFiZTIxMGJhXkEyXkFqcGdeQXVyMTExNDQ2MTI@._V1_FMjpg_UX1000_.jpg' },
+    { title: 'Tales of Demons and Gods', author: 'Mad Snail', url: 'https://m.media-amazon.com/images/I/81wDz65+NOL._AC_UF894,1000_QL80_.jpg' },
+    { title: 'Tower of God', author: 'Slave In Utero', url: 'https://images.justwatch.com/poster/306967058/s718/tower-of-god.jpg' }
+]
 
-        console.log(`Seeded ${insertedUsers.length} users`);
+const insertManga = (title, author, url) => {
+    const text = 'INSERT INTO mangas (title, author, url) VALUES($1, $2, $3)'
+    const values = [title, author, url]
 
-        return {
-            createTable,
-            users: insertedUsers,
-        };
-    } catch (error) {
-        console.error('Error seeding users:', error);
-        throw error;
-    }
+    db.query(text, values, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            console.log(res.rows[0])
+        }
+    })
 }
 
-async function seedInvoices(client) {
-    try {
-        await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+mangas.map(mangas => insertManga(mangas.title, mangas.author, mangas.url))
 
-        // Create the "invoices" table if it doesn't exist
-        const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    customer_id UUID NOT NULL,
-    amount INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date DATE NOT NULL
-  );
-`;
 
-        console.log(`Created "invoices" table`);
+// db.query(`DELETE FROM mangas`)
 
-        // Insert data into the "invoices" table
-        const insertedInvoices = await Promise.all(
-            invoices.map(
-                (invoice) => client.sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-            ),
-        );
 
-        console.log(`Seeded ${insertedInvoices.length} invoices`);
-
-        return {
-            createTable,
-            invoices: insertedInvoices,
-        };
-    } catch (error) {
-        console.error('Error seeding invoices:', error);
-        throw error;
-    }
-}
-
-async function seedCustomers(client) {
-    try {
-        await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-        // Create the "customers" table if it doesn't exist
-        const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS customers (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        image_url VARCHAR(255) NOT NULL
-      );
-    `;
-
-        console.log(`Created "customers" table`);
-
-        // Insert data into the "customers" table
-        const insertedCustomers = await Promise.all(
-            customers.map(
-                (customer) => client.sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-            ),
-        );
-
-        console.log(`Seeded ${insertedCustomers.length} customers`);
-
-        return {
-            createTable,
-            customers: insertedCustomers,
-        };
-    } catch (error) {
-        console.error('Error seeding customers:', error);
-        throw error;
-    }
-}
-
-async function seedRevenue(client) {
-    try {
-        // Create the "revenue" table if it doesn't exist
-        const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS revenue (
-        month VARCHAR(4) NOT NULL UNIQUE,
-        revenue INT NOT NULL
-      );
-    `;
-
-        console.log(`Created "revenue" table`);
-
-        // Insert data into the "revenue" table
-        const insertedRevenue = await Promise.all(
-            revenue.map(
-                (rev) => client.sql`
-        INSERT INTO revenue (month, revenue)
-        VALUES (${rev.month}, ${rev.revenue})
-        ON CONFLICT (month) DO NOTHING;
-      `,
-            ),
-        );
-
-        console.log(`Seeded ${insertedRevenue.length} revenue`);
-
-        return {
-            createTable,
-            revenue: insertedRevenue,
-        };
-    } catch (error) {
-        console.error('Error seeding revenue:', error);
-        throw error;
-    }
-}
-
-async function main() {
-    const client = await db.connect();
-
-    await seedUsers(client);
-    await seedCustomers(client);
-    await seedInvoices(client);
-    await seedRevenue(client);
-
-    await client.end();
-}
-
-main().catch((err) => {
-    console.error(
-        'An error occurred while attempting to seed the database:',
-        err,
-    );
-});
